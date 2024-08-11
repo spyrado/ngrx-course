b1af0960d6562629a7dbd367c80426d5c4dc558e
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-08-11 12:20:20 -0300

    feat(auth.selectors): criando selector de LoggedOut reutilizando o de loggedIn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 import { AuthState } from './auth/reducers';
 import { AuthActions } from './auth/action-types';
 import { AppState } from './reducers';
-import { isLoggedIn } from './auth/auth.selectors';
+import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
 
 @Component({
   selector: 'app-root',
@@ -56,7 +56,7 @@ export class AppComponent implements OnInit {
         .pipe(select(isLoggedIn));
 
       this.isLoggedOut$ = this.store
-        .pipe(select(store => !store['auth'].user))
+        .pipe(select(isLoggedOut))
     }
 
     logout() {
--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -4,3 +4,8 @@ export const isLoggedIn = createSelector(
   state => state['auth'],
   (auth) => !!auth.user
 )
+
+export const isLoggedOut = createSelector(
+  isLoggedIn,
+  loggedIn => !loggedIn
+)
