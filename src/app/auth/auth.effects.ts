ab41d9e0967ed506d8989f3b1965c92aa0b66b58
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-08-13 22:23:39 -0300

    feat(auth.effects): implementando de uma forma mais ngrx/rxjs com type safe utilizando ofType para identificar a ação e ter o type safe dela

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,14 +1,20 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, ofType } from "@ngrx/effects";
+import { AuthActions } from "./action-types";
+import { tap } from "rxjs/operators";
 
 @Injectable()
 export class AuthEffects {
 
   constructor(private actions$: Actions) {
-    this.actions$.subscribe(action => {
-      if(action.type === '[Login Page] User Login') {
-        localStorage.setItem('user', JSON.stringify(action['user']))
-      }
-    });
+    const login$ = this.actions$
+      .pipe(
+        ofType(AuthActions.login),
+        tap(action => {
+          localStorage.setItem('user', JSON.stringify(action.user))
+        })
+      );
+
+    login$.subscribe();
   }
 }
