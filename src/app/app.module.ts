de2c08aa8d5f84ec2d016fbb573b6b853831afdc
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-08-14 22:31:08 -0300

    feat(app.module): adicionando runtimeChecks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,17 @@ const routes: Routes = [
         MatListModule,
         MatToolbarModule,
         AuthModule.forRoot(),
-        StoreModule.forRoot(reducers, { metaReducers }),
+        StoreModule.forRoot(
+          reducers, 
+          { 
+            metaReducers,
+            runtimeChecks: {
+              strictStateImmutability: true,
+              strictActionImmutability: true,
+              strictActionSerializability: true,
+              strictActionTypeUniqueness: true
+            }
+        }),
         StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
         EffectsModule.forRoot([]),
         StoreRouterConnectingModule.forRoot({
