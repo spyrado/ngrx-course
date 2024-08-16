2277f28edbc5849b91a7739c75d602b0370d4f49
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-08-15 23:06:26 -0300

    feat(metaReducers): criando um meta reducer que loga tudo que acontece

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -20,4 +20,13 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
+
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+
+  return (state, action) => {
+    // console.log("state before: ", state);
+    // console.log("action atual: ", action);
+    return reducer(state, action);
+  }
+}
